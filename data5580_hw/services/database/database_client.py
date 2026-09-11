e9e9a6a1fae42381063761d195e2a5a4c73dff4c
Hikaru Izumitani <251102094+hikaru-izumitani@users.noreply.github.com>
2026-09-10 20:51:06 -0600

    Richard Woolston's repository

---
+++ data5580_hw/services/database/database_client.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class BaseModel(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=BaseModel)
+
+
+def init_db(app) -> None:
+    import data5580_hw.services.database.user_sql
+
+    db.init_app(app)
+
+    with app.app_context():
+        try:
+            db.drop_all()
+        except:
+            pass
+        finally:
+            db.create_all()
+            db.session.commit()
